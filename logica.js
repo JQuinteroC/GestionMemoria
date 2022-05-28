504040843401a19c42922851a8f0548d12aaf943
Jose Quintero <jolquinteroc@correo.udistrital.edu.co>
2022-05-27 22:40:39 -0500

    Ajustes en back

--- logica.js
+++ logica.js
@@ -1,5 +1,3 @@
-var tablaProgramas = document.getElementById("tablaProgramas");
-
 var programas = [{
     "nombre": "Word",
     "tamano": 1048576,
@@ -30,46 +28,56 @@ var programas = [{
 },
 ]
 
-for (let i = 0; i < programas.length; i++) {
-    const programa = programas[i];
+function llenarProgramas() {
+    document.getElementById("programas").replaceChildren();
+    for (let i = 0; i < programas.length; i++) {
+        const programa = programas[i];
 
-    var fila = "<tr><td>" + programa.nombre + "</td><td>" + programa.tamano + "</td><td><button class='btn btnEliminar'" + " value='" + i + "'>Encender</button>" + "</td></tr>";
+        var fila = "<tr><td>" + programa.nombre + "</td><td>" + programa.tamano + "</td><td><button class='btn btnEliminar'" + " value='" + i + "'>Encender</button>" + "</td></tr>";
 
-    var btn = document.createElement("TR");
-    btn.innerHTML = fila;
-    document.getElementById("programas").appendChild(btn);
-};
+        var btn = document.createElement("TR");
+        btn.innerHTML = fila;
+        document.getElementById("programas").appendChild(btn);
+    };
 
+    $('#tablaProgramas').on('click', '.btnEliminar', function (event) {
 
+        var $row = $(this).closest("tr"),
+            $tds = $row.find("td");
 
-$('#tablaProgramas').on('click', '.btnEliminar', function (event) {
+        $.each($tds, function () {
+            console.log($(this).text());
+        });
 
-    var $row = $(this).closest("tr"),       // Finds the closest row <tr> 
-        $tds = $row.find("td");             // Finds all children <td> elements
-
-    $.each($tds, function () {               // Visits every single <td> element
-        console.log($(this).text());        // Prints out the text within the <td>
     });
+}
 
-});
+function limpiarMemoria() {
+    var canvas = document.getElementById("memoria");
+    canvas.width = canvas.width;
+}
 
-// var canvas = document.getElementById("memoria");
-// if (canvas.getContext) {
-//     var ctx = canvas.getContext("2d");
+function dibujarMemoria() {
+    var canvas = document.getElementById("memoria");
+    if (canvas.getContext) {
+        var ctx = canvas.getContext("2d");
 
-//     for (let index = 0; index < 16; index++) {
-//         if (index % 2 == 0) {
+        for (let index = 0; index < 16; index++) {
+            if (index % 2 == 0) {
 
-//             ctx.fillStyle = 'black';
-//         } else {
+                ctx.fillStyle = 'black';
+            } else {
 
-//             ctx.fillStyle = 'orange';
-//         }
-//         ctx.fillRect(0, index * 51, 300, 51);
+                ctx.fillStyle = 'orange';
+            }
+            ctx.fillRect(0, index * 51, 300, 51);
+        }
+    }
+}
 
-//     }
+function init() {
+    llenarProgramas();
+    dibujarMemoria();
+}
 
-
-// } else {
-//     alert("No encontrado");
-// }
+init();
